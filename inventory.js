3550a0f2b7c860ba1f5bc59676dca2af14ff89f4
孙国强 <master@pygmalion.click>
2016-08-11 19:09:19 +0900

    Use protobuf of my version, also add express hello world

--- inventory.js
+++ inventory.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const GoogleOAuth = require('gpsoauthnode');
+const google = new GoogleOAuth();
+
+const loginURL = 'https://accounts.google.com/o/oauth2/auth?client_id=848232511240-73ri3t7plvk96pj4f85uj8otdat2alem.apps.googleusercontent.com&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code&scope=openid%20email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email';
+
 /*
 	This example script shows how to work with the getInventory() API call and the splitInventory() function.
 */
@@ -9,35 +14,42 @@ const pogobuf = require('pogobuf'),
 
 const login = new pogobuf.GoogleLogin(),
     client = new pogobuf.Client();
-
+//4/g1FefXs196csIzsFP32JHoWFjq3ppF16Lx2x4oLs1d0
+//oauth2rt_1/PBPLSOQrDvTJow-PJvaNckoJOZUN0R7PAXaRdyImNy4
 // Login to Google and get a login token
-login.loginWithToken('bunnyiscute123@gmail.com', '4/cN7e2-P-TTTZgbz5SSwknZubIpv-KoDtQrd0F6nXzi0')
-    .then(token => {
-        console.log(token);
-        // Initialize the client
-        client.setAuthInfo('google', token);
+//
+new Promise(function(resolve, reject) {
+    google.loginWithAuthCode("4/Vl0oHMwLZRHeHGP5mMLBokTt41VaE73B0WIeOZyiLZ8", function(data, error) {
+        if (error) reject(error);
+        else resolve(data.id_token);
+    });
+})
+.then(token => {
+    console.log(token)
+    // Initialize the client
+    client.setAuthInfo('google', token);
 
-        // Uncomment the following if you want to see request/response information on the console
-        client.on('request', console.dir);
-        client.on('response', console.dir);
+    // Uncomment the following if you want to see request/response information on the console
+    client.on('request', console.dir);
+    client.on('response', console.dir);
 
-        // Perform the initial request
-        return client.init();
-    })
-    .then(() => {
-        // Get full inventory
-        return client.getInventory(0);
-    })
-    .then(inventory => {
-        if (!inventory.success) throw Error('success=false in inventory response');
+    // Perform the initial request
+    return client.init();
+})
+.then(() => {
+    // Get full inventory
+    return client.getInventory(0);
+})
+.then(inventory => {
+    if (!inventory.success) throw Error('success=false in inventory response');
 
-        // Split inventory into individual arrays and log them on the console
-        inventory = pogobuf.Utils.splitInventory(inventory);
-        console.log('Full inventory:', inventory);
+    // Split inventory into individual arrays and log them on the console
+    inventory = pogobuf.Utils.splitInventory(inventory);
+    console.log('Full inventory:', inventory);
 
-        console.log('Items:');
-        inventory.items.forEach(item => {
-            console.log(item.count + 'x ' + pogobuf.Utils.getEnumKeyByValue(POGOProtos.Inventory.Item.ItemId, item.item_id));
-        });
-    })
-    .catch(console.error);
+    console.log('Items:');
+    inventory.items.forEach(item => {
+        console.log(item.count + 'x ' + pogobuf.Utils.getEnumKeyByValue(POGOProtos.Inventory.Item.ItemId, item.item_id));
+    });
+})
+.catch(console.error);
