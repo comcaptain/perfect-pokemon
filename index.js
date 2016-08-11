277c2f522ef7ec4d2fdfe24051e63b8b43bfd49c
孙国强 <master@pygmalion.click>
2016-08-11 20:05:16 +0900

    login to get pokemon json data

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+const pogobuf = require('../pogobuf/pogobuf'),
+    POGOProtos = require('node-pogo-protos');
+
+const googleLogin = new pogobuf.GoogleLogin(),
+    client = new pogobuf.Client();
+
 var express = require('express');
 var session = require('express-session')
 var app = express();
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+
+app.use(express.static("public"));
 
 app.use(session({
 	resave: false,
@@ -8,8 +23,25 @@ app.use(session({
 	secret: 'PUT YOUR SECRET HERE'
 }));
 
-app.get('/', function (req, res) {
-	res.send('Hello World! sessionValue: ' + req.session.sessionValue);
+app.post('/login', function (req, res) {
+	googleLogin.loginWithAuthCode(req.body.googleAuthCode).then(token => {
+	    client.setAuthInfo('google', token);
+	    return client.init();
+	})
+	.then(() => {
+	    return client.getInventory(0);
+	})
+	.then(inventory => {
+	    if (!inventory.success) throw Error('success=false in inventory response');
+
+	    // Split inventory into individual arrays and log them on the console
+	    inventory = pogobuf.Utils.splitInventory(inventory);
+	    res.send(inventory);
+	})
+	.catch(error => {
+		console.error(error);
+		res.send("ERROR: " + error);
+	});
 });
 
 app.get('/setValue', function (req, res) {
