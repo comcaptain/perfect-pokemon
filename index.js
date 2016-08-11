cf8d29b6b11aa5974b1aa616709f5e087635ec74
孙国强 <master@pygmalion.click>
2016-08-11 19:24:32 +0900

    add example for server-side session

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var express = require('express');
+var session = require('express-session')
 var app = express();
 
+app.use(session({
+	resave: false,
+	saveUninitialized: false,
+	secret: 'PUT YOUR SECRET HERE'
+}));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+	res.send('Hello World! sessionValue: ' + req.session.sessionValue);
+});
+
+app.get('/setValue', function (req, res) {
+	req.session.sessionValue = "sessionValue"
+	res.send('Below value is stored: ' + req.session.sessionValue);
 });
 
 app.listen(3000, function () {
