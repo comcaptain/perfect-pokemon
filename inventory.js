8bfe610d41eec5e01716d75bf7f6720c792e8ccc
孙国强 <master@pygmalion.click>
2016-08-11 19:13:37 +0900

    update inventory.js with new protobuf

--- inventory.js
+++ inventory.js
@@ -1,28 +1,28 @@
 'use strict';
-
-const GoogleOAuth = require('gpsoauthnode');
-const google = new GoogleOAuth();
-
-const loginURL = 'https://accounts.google.com/o/oauth2/auth?client_id=848232511240-73ri3t7plvk96pj4f85uj8otdat2alem.apps.googleusercontent.com&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code&scope=openid%20email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email';
-
 /*
-	This example script shows how to work with the getInventory() API call and the splitInventory() function.
+    This example script shows how to work with the getInventory() API call and the splitInventory() function.
 */
-const pogobuf = require('pogobuf'),
+const pogobuf = require('../pogobuf/pogobuf'),
     POGOProtos = require('node-pogo-protos');
 
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-const login = new pogobuf.GoogleLogin(),
+const googleLogin = new pogobuf.GoogleLogin(),
     client = new pogobuf.Client();
-//4/g1FefXs196csIzsFP32JHoWFjq3ppF16Lx2x4oLs1d0
-//oauth2rt_1/PBPLSOQrDvTJow-PJvaNckoJOZUN0R7PAXaRdyImNy4
-// Login to Google and get a login token
-//
+
 new Promise(function(resolve, reject) {
-    google.loginWithAuthCode("4/Vl0oHMwLZRHeHGP5mMLBokTt41VaE73B0WIeOZyiLZ8", function(data, error) {
-        if (error) reject(error);
-        else resolve(data.id_token);
+    console.log("Please visit", pogobuf.GoogleLogin.GET_AUTH_CODE_URL, "to get authentication key.")
+    rl.question('Enter authentication key:', (authCode) => {
+        resolve(authCode)
+        rl.close();
     });
+})
+.then(function(authCode) {
+    return googleLogin.loginWithAuthCode(authCode);
 })
 .then(token => {
     console.log(token)
