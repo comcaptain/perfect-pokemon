4622742b003d5d973c25e9a7f0176964cfb7dc4a
孙国强 <master@pygmalion.click>
2016-08-13 11:28:32 +0900

    show caught time instead of name of pokemon

--- public/js/perfect-pokemon.js
+++ public/js/perfect-pokemon.js
@@ -35,8 +35,8 @@ function renderPokemon(pokemon) {
 	var template = document.getElementById("pokemonTemplate");
 	var pokemonNode = document.importNode(template.content, true);
 	var pokemonID = leftPadZero(pokemon.pokemon_id, 3);
-	pokemonNode.querySelector(".pokemon-name").textContent = pokemonNames[pokemon.pokemon_id];
-	pokemonNode.querySelector(".pokemon-image").style.backgroundImage = "url(./images/" + pokemonID + ".png)";
+	pokemonNode.querySelector(".pokemon-name").textContent = `${timeSince(pokemon.caught_time)} ago`;
+	pokemonNode.querySelector(".pokemon-image").style.backgroundImage = `url(./images/${pokemonID}.png)`;
 	pokemonNode.querySelector(".pokemon-cp").textContent = pokemon.cp;
 	pokemonNode.querySelector(".pokemon-iv-perfection").textContent = pokemon.iv_perfection + "%";
 	return pokemonNode;
@@ -96,4 +96,32 @@ function showUselessPokemons(data) {
     uselessPokemons.sort(function(a, b){return a.pokemon_id - b.pokemon_id});
     data.pokemon = uselessPokemons;
     renderPage(data);
+}
+
+function timeSince(date) {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
 }
