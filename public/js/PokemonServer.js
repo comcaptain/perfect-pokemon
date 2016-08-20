8e107ed1499e94b2d58cf7bdd16719aa82999eb5
孙国强 <master@pygmalion.click>
2016-08-20 12:53:59 +0900

    now pogobuf send us number directly no need to normalize number anymore

--- public/js/PokemonServer.js
+++ public/js/PokemonServer.js
@@ -102,20 +102,12 @@ class PokemonServer {
 	}
 
 	normalizeCreationTime(pokemon) {
-		pokemon.caught_time = new Date(this.normalizeNumber(pokemon.creation_time_ms));
+		pokemon.caught_time = new Date(pokemon.creation_time_ms);
 		return pokemon
 	}
 
 	calculateLevelXP(player) {
 		player.current_level_xp = PLAYER_XP_FOR_EACH_LEVEL[player.level];
-		player.current_level_earned_xp = player.current_level_xp - 
-			(this.normalizeNumber(player.next_level_xp) - this.normalizeNumber(player.experience));
-	}
-
-	normalizeNumber(numberObj) {
-		//`number >>> 0` will convert signed number to unsigner number, e.g. for `(-3).toString(2)`, you'll get `-11`.
-		// but for `(-3 >>> 0).toString(2)`, you'll get its unsigned format value `11111111111111111111111111111101` 
-		// Thanks http://stackoverflow.com/questions/16155592/negative-numbers-to-binary-string-in-javascript
-		return parseInt((numberObj.high >>> 0).toString(16) + (numberObj.low >>> 0).toString(16), 16);
+		player.current_level_earned_xp = player.current_level_xp - (player.next_level_xp - player.experience);
 	}
 }
